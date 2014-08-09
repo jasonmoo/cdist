6e231fca695893abdbce71977832c31d3969c631
Jason Mooberry <jasonmoo@me.com>
2014-08-09 16:18:41 -0400

    adding words with stemming

--- ct.go
+++ ct.go
@@ -1,40 +1,107 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"runtime"
 	"strconv"
-	"unicode/utf8"
+	"strings"
+	"time"
+
+	"github.com/kljensen/snowball/english"
 )
+
+var (
+	words = flag.Bool("w", false, "show word dist")
+	stem  = flag.Bool("stem", false, "snowball stem that shit")
+)
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	flag.Parse()
+}
 
 func main() {
 
-	dict := make(map[rune]int)
+	var inputs []io.Reader
 
-	for {
-		buf := make([]byte, 1<<20)
-
-		n, err := os.Stdin.Read(buf)
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
+	if flag.NArg() > 0 {
+		for _, file_path := range flag.Args() {
+			file, err := os.Open(file_path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			inputs = append(inputs, file)
 		}
-		if n == 0 {
-			break
-		}
-
-		buf = buf[:n]
-
-		for len(buf) > 0 {
-			r, size := utf8.DecodeRune(buf)
-			dict[r]++
-			buf = buf[size:]
-		}
+	} else {
+		inputs = []io.Reader{os.Stdin}
 	}
 
-	for c, ct := range dict {
-		fmt.Printf("%s: %d\n", strconv.QuoteRune(c), ct)
+	start := time.Now()
+
+	switch {
+	case *words:
+
+		dict := make(map[string]int)
+
+		for _, input := range inputs {
+			buf := bufio.NewReader(input)
+			for {
+				line, err := buf.ReadString('\n')
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					log.Fatal(err)
+				}
+
+				words := strings.Fields(line)
+				if *stem {
+					for i, word := range words {
+						words[i] = english.Stem(word, false)
+					}
+				}
+
+				for _, word := range words {
+					dict[word]++
+				}
+			}
+		}
+
+		for word, ct := range dict {
+			fmt.Printf("%s: %d\n", word, ct)
+		}
+
+		fmt.Fprintf(os.Stderr, "%d words counted in %s\n", len(dict), time.Since(start))
+
+	default:
+
+		dict := make(map[rune]int)
+
+		for _, input := range inputs {
+			buf := bufio.NewReader(input)
+			for {
+				r, _, err := buf.ReadRune()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					log.Fatal(err)
+				}
+				dict[r]++
+			}
+		}
+
+		for c, ct := range dict {
+			fmt.Printf("%s: %d\n", strconv.QuoteRune(c), ct)
+		}
+
+		fmt.Fprintf(os.Stderr, "%d runes counted in %s\n", len(dict), time.Since(start))
+
 	}
 
 }
